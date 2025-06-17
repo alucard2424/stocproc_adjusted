2beeb1871f56f2d93ac713b0ecd78e6da3b2a7ba
alucard2424 <108433538+alucard2424@users.noreply.github.com>
2025-06-17 12:45:25 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+# setup.py
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy
+
+extensions = [
+    Extension(
+        "stocproc.stocproc_c",
+        ["stocproc/stocproc_c.pyx"],
+        include_dirs=[numpy.get_include()],
+    )
+]
+
+setup(
+    name="stocproc",
+    version="1.1.2",
+    description="Stochastic Processes in Python",
+    author="Your Name",
+    packages=["stocproc", "stocproc.processes", "stocproc.methods"],
+    ext_modules=cythonize(extensions, language_level=3),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "matplotlib",
+        "fastcubicspline",
+    ],
+)
